d36263d31e5b3ece9d2883ac98af31d9b38dc990
laishi <laishi886@hotmail.com>
2025-07-05 21:08:22 +0800

    temp

--- assets/js/pageGrid.js
+++ assets/js/pageGrid.js
@@ -17,14 +17,45 @@ const templates = [
     "16-clouds-html5-multipurpose-landing-page-template",
     "17-coffee-shop-free-html5-template",
     "18-creative-free-responsive-html5-business-template",
-    "19-darktouch-corporate-portfolio-bootstrap-responsive-web-template"
+    "19-darktouch-corporate-portfolio-bootstrap-responsive-web-template",
+    "20-delite-music-html5-bootstrap-responsive-web-template",
+    "21-eat-restaurant-bootstrap-html5-template",
+    "22-elegant-free-multi-purpose-bootstrap-responsive-template",
+    "23-enlive-corporate-free-html5-bootstrap-web-template",
+    "24-fit-healthy-fitness-and-gym-html5-bootstrap-theme",
+    "25-fitness-zone-html5-bootstrap-responsive-web-template",
+    "26-frames-corporate-bootstrap-free-html5-template",
+    "27-free-bootstrap-template-real-estate-my-home",
+    "28-free-bootstrap-template-restaurant-website-treehut",
+    "29-free-bootstrap-template-rockline-business",
+    "30-free-portfolio-html5-responsive-website-sam",
+    "getdoctor-free-bootstrap-responsive-website-template",
+    "golden-hotel-free-html5-bootstrap-web-template",
+    "grand-free-bootstrap-responsive-website-template",
+    "green-corp-flat-free-responsive-mobile-website",
+    "iam-html5-responsive-portfolio-resume-template",
+    "iclick-photography-bootstrap-free-website-template",
+    "idata-hosting-free-bootstrap-responsive-website-template",
+    "ideal-interior-design-free-bootstrap-website-template",
+    "john-bootstrap-one-page-html5-free-resume-template",
+    "johndoe-portfolio-resume-bootstrap-template",
+    "line-free-app-landing-page-responsive-web-template",
+    "lovely-wedding-bootstrap-free-website-template",
+    "me-resume-personal-portfolio-responsive-template",
+    "mobile-app-free-one-page-responsive-html5-landing-page",
+    "relax-interior-free-bootstrap-responsive-website-template",
+    "rocket-business-bootstrap-free-responsive-web-theme",
+    "skytouch-onepage-bootstrap-responsive-web-template",
+    "smart-interior-designs-html5-bootstrap-web-template",
+    "speed-hosting-bootstrap-free-html5-template"
 ];
+
 
 class SliderDetailGrid {
     constructor() {
-        this.templates = templates;
-        this.tempPath = "./qinghua-template/spa/";
-        this.tempLink = "/temp.html";
+        this.templates = templates.slice(0, 19);
+        this.tempPath = "./assets/qinghua-template/spa/";
+        this.tempLink = "./assets/pages/temp.html";
         this.mainPage = document.querySelector(".mainPage");
         this.pageSlider = document.getElementById("pageSlider");
         this.detailGrids = null;
@@ -34,11 +65,22 @@ class SliderDetailGrid {
     }
 
     init() {
+        this.setAssets()
         this.createGridPage();
         this.createDetailPage();
         this.createDetailGrids();
         this.navEvents();
         this.bindEvents();
+    }
+
+    setAssets() {
+        const currentPath = window.location.href;
+        const isRoot = currentPath === window.location.origin + '/' || currentPath === window.location.origin;
+        const isDetailView = currentPath === window.location.origin + '/#detailView';
+
+        if (!isRoot && !isDetailView) {
+            this.tempPath = "../qinghua-template/spa/";
+        }
     }
 
     bindEvents() {
@@ -163,18 +205,18 @@ class SliderDetailGrid {
     injectImageIntoGrid(grid, linkurl, imgurl, info) {
         const link = document.createElement("a");
         link.href = linkurl;
-        link.target = "_blank";
+        // link.target = "_blank";
         link.addEventListener("click", (e) => e.preventDefault());
 
         const img = document.createElement("img");
         img.src = imgurl;
         img.loading = "lazy";
         link.appendChild(img);
-        
+
         // Add imgMask overlay
         const imgMask = document.createElement("div");
         imgMask.className = "imgMask";
-        
+
         const gridInfo = document.createElement("div");
         gridInfo.className = "gridInfo";
         const keywords = info.split("/").slice(-2, -1)[0].split("-");
@@ -184,7 +226,7 @@ class SliderDetailGrid {
             keywordDiv.innerHTML = `<h3>${keyword}</h3>`;
             gridInfo.appendChild(keywordDiv);
         });
-        
+
         grid.appendChild(link);
         grid.appendChild(gridInfo);
         grid.appendChild(imgMask);
