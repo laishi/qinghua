e0a95e6fc48d9a173f8c07ee6a1804f5e94919bb
laishi <laishi886@hotmail.com>
2025-07-11 13:26:34 +0800

    qinghua-update font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.qinghua.cloud',
+  integrations: [sitemap()],
   base: '/',           // 保持为 '/'，不要改成 './'
   outDir: 'dist',      // 构建目录，和 Actions 中一致
 });
