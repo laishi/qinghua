18f8505f7c464bfe1a57d78d96390a7e3c287a99
laishi <laishi886@hotmail.com>
2025-08-28 22:41:37 +0800

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import netlify from '@astrojs/netlify';
 import fs from 'fs';
 import path from 'path';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://www.qinghua.cloud',
   base: '/',
   outDir: 'dist',
   output: 'static',
-  adapter: netlify(),
+  adapter: cloudflare(),
 
   hooks: {
     'astro:build:done': async ({ outDir }) => {
@@ -28,3 +30,6 @@ export default defineConfig({
     },
   },
 });
+
+
+import { defineConfig } from 'astro/config';
