2471c91789d23d8a5acd084e15ef0c0f2c1f5e37
laishi <laishi886@hotmail.com>
2025-08-28 23:01:12 +0800

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,24 +11,6 @@ export default defineConfig({
   outDir: 'dist',
   output: 'static',
   adapter: cloudflare(),
-
-  hooks: {
-    'astro:build:done': async ({ outDir }) => {
-      try {
-        const source = path.resolve('sitemap.xml'); // 根目录的 sitemap.xml
-        const dest = path.join(outDir, 'sitemap.xml'); // dist 目录
-
-        if (fs.existsSync(source)) {
-          fs.copyFileSync(source, dest);
-          console.log('✅ Sitemap 已成功复制到 dist 目录');
-        } else {
-          console.warn('⚠️ 根目录未找到 sitemap.xml 文件');
-        }
-      } catch (err) {
-        console.error('❌ 复制 sitemap.xml 出错:', err);
-      }
-    },
-  },
 });
 
 
